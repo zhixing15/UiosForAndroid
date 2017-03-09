7674cbfeb078871ecdff14390ed1c7f1fe07b0ac
Jay <qiu.jay892@gmail.com>
2017-03-09 16:21:25 +0800

    Initial commit

---
+++ app/src/main/java/android/ui/MainActivity.java
@@ -0,0 +1,30 @@
+package android.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void testClick(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder
+                .setTitle("IosAlertDialog")
+                .setMessage("IosAlertDialogMessage")
+                .setPositiveButton("OK", null)
+                .setNegativeButton("Cancel", null)
+                .setNeutralButton("No", null)
+                .show();
+
+//        startActivity(new Intent(this,MainActivity.class));
+    }
+}
